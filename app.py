c64d693fa784b767c211288d545a26c52bfb165f
charlzk05 <charliezkiecharlzk@gmail.com>
2023-02-07 20:19:32 +0800

    To be continued!

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os, requests, json
 
 app = Flask(__name__)
 port = 3000
@@ -11,7 +12,16 @@ def main():
 def search():
     if request.method == "GET":
         name = request.args.get("name")
-        return render_template("search.html", name=name)
+        url = f"https://imdb-movies-web-series-etc-search.p.rapidapi.com/{name}.json"
+        
+        response = requests.get(url, headers={
+            "X-RapidAPI-Key": os.environ["rapidAPIKey"],
+	        "X-RapidAPI-Host": os.environ["rapidAPIHost"]
+        })
+        
+        loads = json.loads(response.text)
+        
+        return render_template("search.html", name=name, result=loads["d"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
