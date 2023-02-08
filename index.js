61940e782a5fd07d45682449b209521c299e207d
charlzk05 <charliezkiecharlzk@gmail.com>
2023-02-08 13:22:50 +0800

    using node js

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "views")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.get("/search", (req, res) => {
+    const name = req.query.name;
+    const options = {
+        method: "GET",
+        url: `https://imdb-movies-web-series-etc-search.p.rapidapi.com/${name}.json`,
+        headers: {
+            "X-RapidAPI-Key": process.env["rapidAPIKey"],
+            "X-RapidAPI-Host": process.env["rapidAPIHost"]
+        }
+    };
+
+    axios.request(options).then((response) => {
+        res.render("search", {
+            name: name,
+            results: response.data["d"]
+        });
+    }).catch((err) => {
+        res.sendStatus(500);
+        console.log(err.message);
+    });
+});
+
+app.listen(port, () => {
+    console.log("Express server is listening to " + port);
+});
